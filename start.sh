6669c6f409e1626bb5778b537ca46e8eed140260
rafahmarinho <rafael.marinho96@hotmail.com>
2020-11-24 20:47:53 -0300

    fix npm start

--- start.sh
+++ start.sh
@@ -3,3 +3,5 @@ echo "[Script - start.sh] Instalando pacote do express."
 yarn
 echo "[Script - start.sh] Iniciando servidor nodeJS."
 npx nodemon server.js
+echo "[Script - start.sh] Iniciando servidor react."
+react-scripts start
